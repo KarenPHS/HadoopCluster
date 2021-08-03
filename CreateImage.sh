5f9b7c157e6518d0fecf66acb05514bad3c13473
KarenPHS <hsaun8169@gmail.com>
2021-08-03 15:37:28 +0800

    change user

--- CreateImage.sh
+++ CreateImage.sh
@@ -8,3 +8,4 @@ cd /home/hsuan8169/HadoopCluster/HistoryServer
 docker image build -t hsuan8169/historyserver .
 cd /home/hsuan8169/HadoopCluster/Worker1
 docker image build -t hsuan8169/workers .
+cd /home/hsuan8169/HadoopCluster/
