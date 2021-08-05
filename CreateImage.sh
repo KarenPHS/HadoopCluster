2471c5e4eaac229eb5f7ad6eecfe9a683f2813b2
KarenPHS <hsaun8169@gmail.com>
2021-08-05 18:25:03 +0800

    HA format

--- CreateImage.sh
+++ CreateImage.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /home/hsuan8169/HadoopCluster/Active_NN
-docker image build -t hsuan8169/active_nn .
+docker image build -t hsuan8169/active-nn .
 cd /home/hsuan8169/HadoopCluster/Active_RM
-docker image build -t hsuan8169/active_rm .
+docker image build -t hsuan8169/active-rm .
 cd /home/hsuan8169/HadoopCluster/HistoryServer
 docker image build -t hsuan8169/historyserver .
 cd /home/hsuan8169/HadoopCluster/Worker1
